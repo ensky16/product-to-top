1b9cc58a07ea0f7fbc7346dd16887cc69a96f2d5
ensky <ensky16@163.com>
2021-04-23 23:17:19 +0800

    this will make the self-define order after the original order

--- product-to-top.php
+++ product-to-top.php
@@ -15,8 +15,12 @@
  */
 
 
+//remove_action('woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open');
+//add_action( 'woocommerce_before_shop_loop','make_prodcut_to_the_top', -1);
 
-add_action( 'woocommerce_before_shop_loop','make_prodcut_to_the_top' );
+add_action( 'woocommerce_after_shop_loop','make_prodcut_to_the_top' );
+
+//add_filter( 'woocommerce_before_shop_loop','make_prodcut_to_the_top' );
 
 function make_prodcut_to_the_top()
 {
